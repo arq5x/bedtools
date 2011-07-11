41517ea9928fc043d374e388b722561b6fcf7119
Aaron <aaronquinlan@gmail.com>
2011-07-11 14:14:14 -0400

    Makefile tweaks.

--- src/utils/bits/bits.h
+++ src/utils/bits/bits.h
@@ -6,10 +6,10 @@
 uint32_t uint32_bsearch(uint32_t key, uint32_t *D, uint32_t D_size, int lo, uint32_t hi);
 
 uint32_t count_intersections_bsearch_seq(uint32_t *A_starts,
-                                    uint32_t *A_ends, 
-									uint32_t A_size,
-									uint32_t *B_starts,
-									uint32_t *B_ends,  
-                                    uint32_t B_size );
+                                         uint32_t *A_ends, 
+									     uint32_t A_size,
+									     uint32_t *B_starts,
+									     uint32_t *B_ends,  
+                                         uint32_t B_size);
 
 #endif
